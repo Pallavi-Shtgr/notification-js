0029c099de019619a23d643d2ead0406debb81dc
Pallavi <pallavishettigar03@gmail.com>
2024-01-08 23:41:46 +0530

    upload file

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-let isOnline = true;
+const popup = document.querySelector(".popup"),
+wifiIcon= document.querySelector(".icon i "),
+popupTitle = document.querySelector(".popup .title"),
+popupDesc = document.querySelector(".desc");
+let isOnline = true, setIntervalId, timer =10 ;
 
 const checkConnection = async () => {
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-        isOnline = response.status >= 200 && response. status < 300;
+        isOnline = response.status >= 200 && response.status < 300;
         console.log(response);
 
     } catch (error) {
-        console.error(error);
+        isOnline = false;
     }
+    timer=10;
+    clearInterval(intervalId)
+    handlePopup(isOnline);
 }
 
-setInterval(checkConnection, 3000);
+const handlePopup = (status) => {
+    if (status) {
+        wifiIcon.className=  "uil uil-wifi"
+        popupTitle.innerText="Restored Connection";
+        popupDesc.innerHTML= "Your devicec is now successfully connected to the internet";
+        popup.classList.add("online");
+        return setTimeout(() => popup.classList.remove("show",),2000);
+    }
+    wifiIcon.className=  "uil uil-wifi-slash"
+    popupTitle.innerText="Restored Connection";
+    popupDesc.innerHTML= "Your devicec is now successfully connected to the internet";
+        popup.className= "popup show ";
+
+        intervalId = setIntervalId(() => {
+            timer--;
+            if (timer===0) checkConnection();
+            popup.querySelector(".desc b").innerText = timer;
+    },1000 )
+}
+
+
+
+setInterval(() => isOnline && checkConnection, 3000);
